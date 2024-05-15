396030b8538ef6b16a2a975ec7bce6025d466e0f
Ahmed <7LM112121@gmail.com>
2024-05-15 23:36:42 +0300

    fixed some proplems

--- main.js
+++ main.js
@@ -135,6 +135,17 @@ async function quoteListen() {
 }
 
 function copyQuote() {
+  const Toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.onmouseenter = Swal.stopTimer;
+      toast.onmouseleave = Swal.resumeTimer;
+    },
+  });
   const textToCopy = quoteText.innerText;
 
   // Create a temporary input element
